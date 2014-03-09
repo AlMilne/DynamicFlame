13f48da3b17cbc429722a2b51db58069b6e378df
LazyChump <alasdair.milne@yahoo.co.uk>
2014-03-09 18:53:24 +0800

    - Hibernate generated model classes and mapping files for images

---
+++ DynamicFlame/src/main/java/org/au/dynamicflame/photos/dao/PhotosDAO.java
@@ -0,0 +1,23 @@
+/*
+ * Module: DynamicFlame, Project: Unity Digital
+ * Copyright (C) Aviva UK Life, All rights reserved
+ */
+package org.au.dynamicflame.photos.dao;
+
+import java.util.List;
+
+import org.au.dynamicflame.model.Image;
+
+
+/**
+ * PhotosDAO.java - TODO Alasdair COMMENT MISSING.
+ *
+ * @author Alasdair
+ * @since 09/03/2014
+ */
+public interface PhotosDAO {
+
+    public List<Image> getCategories();
+
+
+}
